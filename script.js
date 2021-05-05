c2594aef007a5b4be38b2a5af01c7bbcbdca3287
thalesfelippe <72764998+thalesfelippe@users.noreply.github.com>
2021-05-05 16:21:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ const quizData = [
     }
 ];
 
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEL = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -53,25 +55,54 @@ const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
-let currentQuestion = 0;
+let currentQuiz = 0;
+let score = 0
 
 loadQuiz();
 
 function loadQuiz() {
-    const currentQuizData = quizData
-    [currentQuestion]
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
     questionEL.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
+}
 
-    currentQuestion++;
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+       answerEl.checked = false;
+    });
 }
 
 submitBtn.addEventListener('click', () => {
-    currentQuestion++;
-    
+    // check to see the answer
+    const answer = getSelected();
 
-    loadQuiz();
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+             <button onclick="location.reload()">Reload</button>`
+        }
+    }
 });
